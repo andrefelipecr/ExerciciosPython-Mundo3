be781293f02337e46b288d18bfb553f572df5734
André Felipe <contatoandrefelipecr@gmail.com>
2025-01-27 18:29:30 -0300

    Módulo para cores no terminal

---
+++ cores.py
@@ -0,0 +1,12 @@
+# Códigos ANSI para cores
+VOLTA = "\033[A"
+LIMPA = "\033[2K"
+RESET = "\033[0m"
+RED = "\033[31m"
+MAGENTA = "\033[35m"
+GREEN = "\033[32m"
+YELLOW = "\033[33m"
+CYAN = "\033[36m"
+
+def limpa():
+    print(VOLTA,LIMPA,VOLTA)
