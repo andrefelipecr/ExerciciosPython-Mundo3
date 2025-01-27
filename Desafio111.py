30aae54fb41a95659b914b79b0a4ca3801285132
André Felipe <contatoandrefelipecr@gmail.com>
2025-01-27 18:34:58 -0300

    Modularização - 4ª versão

---
+++ Desafio111.py
@@ -0,0 +1,8 @@
+from Desafio111.utilidades import dinheiro
+from cores import *
+
+reais = float(input(f'{YELLOW}Insira o preço: {GREEN}R${RESET}'))
+limpa()
+por_cento = int(input(f'{YELLOW}Digite o acréscimo ou o desconto:{RESET} '))
+limpa()
+dinheiro.analise(reais, por_cento)
