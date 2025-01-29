9c5d4fcb92d6b0f6a1e4b972f6482a857c8f3ee1
André Felipe <contatoandrefelipecr@gmail.com>
2025-01-29 08:20:56 -0300

    Update Desafio107.py

--- Desafio107.py
+++ Desafio107.py
@@ -1,10 +1,10 @@
-from Desafio107 import dinheiro
+from Desafio107.dinheiro import ajuste, dobro, metade
 from cores import *
 
 reais = float(input(f'{YELLOW}Insira o preço: {GREEN}R${RESET}'))
 
-print(f'A metade de R${reais:.2f} é R${dinheiro.metade(reais):.2f}')
-print(f'O dobro de R${reais:.2f} é R${dinheiro.dobro(reais):.2f}')
+print(f'A metade de R${reais:.2f} é R${metade(reais):.2f}')
+print(f'O dobro de R${reais:.2f} é R${dobro(reais):.2f}')
 por_cento = int(input(f'{YELLOW}Digite o ajuste:{RESET} '))
 limpa()
-print(dinheiro.ajuste(reais, por_cento))
+print(ajuste(reais, por_cento))
